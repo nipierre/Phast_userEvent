c3acb4a03489f24af47b61f7501e577dd8ac2052
npierre <npierre@macbook-pro-de-npierre.home>
2018-01-05 17:02:50 +0100

    Fix.

--- LCAnalysis.cc
+++ LCAnalysis.cc
@@ -669,8 +669,7 @@ void LCAnalysis::FindHadrons(PaEvent& ev)
       const PaTPar& ParamMu0  = Mu0.ParInVtx(fiBPV);
       fCovMu0 = ParamMu0(5,5);
 
-      // fValidMu=IsMu0Valid(ParamMu0);
-      fValidMu = 1;
+      fValidMu=IsMu0Valid(ParamMu0);
     }
 
     // cout << ", Vx : " << v.X() << ", Vy : " << v.Y() << ", Vz : " << v.Z() << ", mu' rec. :" << v.iMuPrim();
